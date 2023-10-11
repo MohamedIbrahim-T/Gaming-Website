c6b206cd94bf459b3725725a2365d3a5b1908cc4
Jamal <miibrahim111104@gmail.com>
2023-10-12 02:44:50 +0530

    add all

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function playSound(audioName,loop) {
-    let audio = new Audio(audioName);
-    audio.loop = loop;
-    audio.play();
-}
-playSound("PUBG Mobile 2.0 update Theme - Run Away BGM Official BGMI ( by Alan Walker ) 2022.mp3",true)
+$(document).ready(function(){
+$('#icon').click(function(){
+$('.nav-links').toggleClass('show')
+});
+});
